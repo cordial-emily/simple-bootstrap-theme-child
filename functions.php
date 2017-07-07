af0096c6554cbbffc2736bb1062b523a9ac10410
Emily Schleiner <cordial.emily@gmail.com>
2017-07-07 15:08:26 -0700

    testing style.css

--- functions.php
+++ functions.php
@@ -1,14 +1,15 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'simple-bootstrap-theme-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'simple_bootstrap_styles'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'simple-bootstrap-theme-child',
+    wp_enqueue_style( 'simple-bootstrap-child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
