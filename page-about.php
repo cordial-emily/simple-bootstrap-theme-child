7a7ea3a0d354bcef423e896036090d564846f011
Emily Schleiner <cordial.emily@gmail.com>
2018-02-20 14:15:53 -0800

    add location to about page

--- page-about.php
+++ page-about.php
@@ -5,6 +5,20 @@ Template Name: About
 */
 get_header();
  ?>
+ <div class="main jumbotron" style="background-color: #ffffff; margin-bottom: 0px; padding-bottom: 0px; padding-top: 0px;">
+     <div class="container">
+       <div class="row">
+         <div class="col-sm-4 overlay text-right">
+           <br>
+           <h3>Where are we?</h3>
+           <p style="font-size: 16px;">We are located in sunny Davis, California. Sometimes we travel by bicycle to meet with local clients.</p>
+         </div>
+         <div class="col-sm-8 text-center" style="overflow:hidden;">
+           <img longdesc="cordial-emily.com" src="https://paralleldesign.net/wp-content/uploads/2018/02/davisCA-parallel-design.jpg" alt="Emily Schleiner" height="auto" width="600px">
+         </div>
+       </div>
+     </div>
+   </div>
  <div class="banner">
 
     <div class="middle">
@@ -25,25 +39,24 @@ get_header();
                    <img class="img-fluid" style="padding-top: 5em;" src="https://paralleldesign.net/wp-content/uploads/2017/08/Lotus-laptop.png" alt="ParallelDesign" height="auto" width="100%">
                  </div>
               </div> <!--row-->
-              <div class="row">
-                <hr>
-              </div>
+
         </div><!-- container cvmar -->
       </section><!-- pspace -->
     </div><!--middle-->
   <!--</div><!--end gradient_container-->
 </div>
-<div class="main jumbotron" style="background-color: #f4f5f9; margin-bottom: 0px; padding-bottom: 0px; padding-top: 0px;">
-    <div class="container">
+  <div class="main jumbotron" style="background-color: #f4f5f9; margin-bottom: 0px; padding-bottom: 0px; padding-top: 0px;">
+      <div class="container">
+
       <div class="row">
         <div class="col-sm-6 text-center">
-          <img longdesc="cordial-emily.com" src="https://paralleldesign.net/wp-content/uploads/2017/08/emily-schleiner-2016.jpg" alt="ParallelDesign" height="auto" width="300px">
+          <img longdesc="cordial-emily.com" src="https://paralleldesign.net/wp-content/uploads/2017/08/emily-schleiner-2016.jpg" alt="Emily Schleiner" height="auto" width="300px">
         </div>
         <div class="col-sm-6 overlay">
           <br>
           <h3>Emily Schleiner <small class="text-muted">Founder, Web Developer</small></h3>
           <p style="font-size: 16px;">Seven years ago, Emily got her Masters in performance and interactive art, and found herself enamored of the process of making her portfolio website. Many web and graphic design projects later, she has grown to be an expert on building Wordpress Themes. <br>
-            She works dedicatedly to enhance the user experience of each development project she works on and has a keen interest in responsive web design and front-end web development libraries; she works hard to continually update her skills.</p>
+            She works dedicatedly to enhance the user experience of each development project she works on and has a keen interest in responsive web design and front-end web development libraries.</p>
         </div>
 
       </div>
